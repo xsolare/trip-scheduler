a3c4b5313801a29ae113a4aef2b6750cd7e69343
injurka <injurka@gmail.com>
2025-08-06 23:39:17 +0400

    yep yep

--- apps/client/src/shared/types/dts/env.d.ts
+++ apps/client/src/shared/types/dts/env.d.ts
@@ -1,16 +1,13 @@
-/// <reference types="@rsbuild/core/types" />
-
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-
-  const component: DefineComponent<object, object, any>
-  export default component
-}
+/// <reference types="vite-plugin-pwa/client" />
+/// <reference types="vite/client" />
 
 interface ImportMetaEnv {
   readonly VITE_APP_MOCK_MODE: string
   readonly VITE_APP_TRPC_MODE: string
   readonly VITE_APP_TRPC_URL: string
   readonly VITE_APP_REQUEST_THROTTLE: boolean
+}
 
+interface ImportMeta {
+  readonly env: ImportMetaEnv
 }
